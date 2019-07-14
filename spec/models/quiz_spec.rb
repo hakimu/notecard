e56197660b2eb5c54c78762a28e33e96f13df40f
Hakimu Jackson <hakimu@newrelic.com>
2019-07-14 15:58:49 -0700

    Update quiz spec.  All model specs pass. User needs refactoring.

--- spec/models/quiz_spec.rb
+++ spec/models/quiz_spec.rb
@@ -2,8 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Quiz, type: :model do
   context 'when given valid attributes' do
-    it 'creates a valid quiz' do
+    it 'creates a valid quiz when given notes' do
+      note = create(:note)
       quiz = build(:quiz)
+      quiz.notes << note
       expect { quiz.save }.to change { Quiz.count }.by(1)
     end
   end
