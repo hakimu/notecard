a64e29d4f14246697b0cd2f2b35f052b91ca91a9
Hakimu Jackson <hakimu@newrelic.com>
2019-07-14 21:44:55 -0700

    WIP: Spec and code work. Need refactor

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -2,7 +2,7 @@ class Quiz < ApplicationRecord
   DIFFICULTY = [1,2,3,4,5]
   has_many :notes
   belongs_to :user
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
   validates :notes, presence: true
   validates :questions, presence: true
   validates :difficulty, inclusion: {
--- spec/models/quiz_spec.rb
+++ spec/models/quiz_spec.rb
@@ -9,4 +9,43 @@ RSpec.describe Quiz, type: :model do
       expect { quiz.save }.to change { Quiz.count }.by(1)
     end
   end
+  context 'when given invalid attributes' do
+    it 'is invalid without a name' do
+      note = create(:note)
+      quiz = build(:quiz, name: nil)
+      quiz.notes << note
+      quiz.save
+      expect(quiz).to_not be_valid
+      expect(quiz.errors.full_messages).to include('Name can\'t be blank')     
+    end
+    it 'is invalid when the quiz name is not unique' do
+      note = create(:note)      
+      quiz = build(:quiz)
+      quiz.notes << note
+      quiz.save
+      another_quiz = build(:quiz, name: 'My Quiz')
+      another_quiz.notes << note 
+      another_quiz.save
+      expect(another_quiz.errors.full_messages).to include('Name has already been taken')
+    end
+    it 'is invalid without a note' do
+      quiz = build(:quiz)
+      quiz.save
+      expect(quiz.errors.full_messages).to include('Notes can\'t be blank')
+    end
+    it 'is invalid without a user' do
+      note = create(:note)
+      quiz = build(:quiz, user_id: nil)
+      quiz.notes << note
+      quiz.save
+      expect(quiz.errors.full_messages).to include('User must exist')
+    end
+    it 'is invalid when there is not a difficulty' do
+      note = create(:note)
+      quiz = build(:quiz, difficulty: nil)
+      quiz.notes << note
+      quiz.save
+      expect(quiz.errors.full_messages).to include('Difficulty must be between 1 and 5')
+    end
+  end
 end
