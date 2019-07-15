2941186e1696f058f65efe8d9e23a9841a10da07
Hakimu Jackson <hakimu@newrelic.com>
2019-07-14 21:41:26 -0700

    WIP: Update User spec. Need refactoring

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,6 +7,17 @@ RSpec.describe User, type: :model do
         user = create(:user)
         expect(user).to be_valid
       end
+      it 'is able to have a note associated with it' do
+        user = create(:user)
+        user.notes.create(method: 'def', code_sample: 'def test_method end', difficulty: 1)
+        expect(user.notes.count).to eq(1)
+      end
+      it 'is able to have multiple notes associated with it' do
+        user = create(:user)
+        create(:note, user_id: user.id)  
+        create(:note, user_id: user.id)
+        expect(user.notes.count).to eq(2)
+      end
     end
     context 'when given INVALID attributes' do
       it 'is invalid without a username' do
@@ -19,6 +30,11 @@ RSpec.describe User, type: :model do
         user.save
         expect(user.errors[:email]).to include('can\'t be blank', 'is invalid')
       end
+			it 'is invalid with an invalid email address' do
+				user = build(:user, email: 'h@')
+				user.valid?
+				expect(user.errors.full_messages).to include('Email is invalid')
+			end
 			it 'is invalid with a duplicate username' do
 				create(:user, username: 'hakimu')
 				another_user = FactoryBot.build(:user, username: 'hakimu')
@@ -31,31 +47,11 @@ RSpec.describe User, type: :model do
         another_user.valid?
         expect(another_user.errors[:email]).to include('has already been taken')
       end
+			it 'is invalid with a password less than 6 characters' do
+				user = FactoryBot.build(:user, password: 'pass')
+				user.valid?
+				expect(user.errors[:password]).to include('is too short (minimum is 6 characters)')
+			end
     end
   end
-
-  it 'is able to have a note associated with it' do
-    user = FactoryBot.create(:user)
-    note = user.notes.create(method: 'def', code_sample: 'def test_method end', difficulty: 1)
-    expect(user.notes.count).to eq(1)
-  end
-  it 'is able to have multiple notes associated with it' do
-    user = create(:user)
-    note_1 = create(:note, user_id: user.id)  
-    note_2 = create(:note, user_id: user.id)
-    expect(user.notes.count).to eq(2)
-  end
-
-  it 'is invalid with an invalid email address' do
-    user = FactoryBot.build(:user, email: 'h@')
-    user.valid?
-    expect(user.errors.full_messages).to include('Email is invalid')
-  end
-
-  it 'is invalid with a password less than 6 characters' do
-    user = FactoryBot.build(:user, password: 'pass')
-    user.valid?
-    expect(user.errors[:password]).to include('is too short (minimum is 6 characters)')
-  end
-
 end
